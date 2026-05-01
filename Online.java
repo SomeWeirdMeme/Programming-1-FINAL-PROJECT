c4b91705ff762a7574ee0108f5e37b53677b661d
SomeWeirdMeme <ant.osuant@gmail.com>
2026-05-01 11:47:20 -0400

    Finalized the skeleton for the project

--- Online.java
+++ Online.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Online
+public class Online extends Game
 {
     // instance variables - replace the example below with your own
     private int x;
--- SinglePlayer.java
+++ SinglePlayer.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class SinglePlayer
+public class SinglePlayer extends Game
 {
     // instance variables - replace the example below with your own
     private int x;
